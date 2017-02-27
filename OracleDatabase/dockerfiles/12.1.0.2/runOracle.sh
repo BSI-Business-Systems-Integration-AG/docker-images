d28424bf3734360f12ec36591d0aa1250b692bf7
Michael Schaufelberger <Michael.Schaufelberger@bsi-software.com>
2017-02-27 14:32:57 +0100

    allow running runOracle.sh without wait at the end, by passing "no-wait"

--- OracleDatabase/dockerfiles/12.1.0.2/runOracle.sh
+++ OracleDatabase/dockerfiles/12.1.0.2/runOracle.sh
@@ -2,13 +2,13 @@
 # LICENSE CDDL 1.0 + GPL 2.0
 #
 # Copyright (c) 1982-2016 Oracle and/or its affiliates. All rights reserved.
-# 
+#
 # Since: November, 2016
 # Author: gerald.venzl@oracle.com
 # Description: Runs the Oracle Database inside the container
-# 
+#
 # DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS HEADER.
-# 
+#
 
 ########### Move DB files ############
 function moveFiles {
@@ -23,7 +23,7 @@ function moveFiles {
 
    # oracle user does not have permissions in /etc, hence cp and not mv
    cp /etc/oratab $ORACLE_BASE/oradata/dbconfig/$ORACLE_SID/
-   
+
    symLinkFiles;
 }
 
@@ -33,16 +33,16 @@ function symLinkFiles {
    if [ ! -L $ORACLE_HOME/dbs/spfile$ORACLE_SID.ora ]; then
       ln -s $ORACLE_BASE/oradata/dbconfig/$ORACLE_SID/spfile$ORACLE_SID.ora $ORACLE_HOME/dbs/spfile$ORACLE_SID.ora
    fi;
-   
+
    if [ ! -L $ORACLE_HOME/dbs/orapw$ORACLE_SID ]; then
       ln -s $ORACLE_BASE/oradata/dbconfig/$ORACLE_SID/orapw$ORACLE_SID $ORACLE_HOME/dbs/orapw$ORACLE_SID
    fi;
-   
+
    if [ ! -L $ORACLE_HOME/network/admin/tnsnames.ora ]; then
       ln -s $ORACLE_BASE/oradata/dbconfig/$ORACLE_SID/tnsnames.ora $ORACLE_HOME/network/admin/tnsnames.ora
    fi;
 
-   # oracle user does not have permissions in /etc, hence cp and not ln 
+   # oracle user does not have permissions in /etc, hence cp and not ln
    cp $ORACLE_BASE/oradata/dbconfig/$ORACLE_SID/oratab /etc/oratab
 
 }
@@ -84,7 +84,7 @@ EOF
 
 # Check whether container has enough memory
 # Github issue #219: Prevent integer overflow,
-# only check if memory digits are less than 11 (single GB range and below) 
+# only check if memory digits are less than 11 (single GB range and below)
 if [ `cat /sys/fs/cgroup/memory/memory.limit_in_bytes | wc -c` -lt 11 ]; then
    if [ `cat /sys/fs/cgroup/memory/memory.limit_in_bytes` -lt 2147483648 ]; then
       echo "Error: The container doesn't have enough memory allocated."
@@ -113,7 +113,7 @@ else
      echo "Error: The ORACLE_SID must only be up to 12 characters long."
      exit 1;
   fi;
-  
+
   # Check whether SID is alphanumeric
   # Github issue #246: Cannot start OracleDB image
   if [[ "$ORACLE_SID" =~ [^a-zA-Z0-9] ]]; then
@@ -135,24 +135,24 @@ fi;
 # Check whether database already exists
 if [ -d $ORACLE_BASE/oradata/$ORACLE_SID ]; then
    symLinkFiles;
-   
+
    # Make sure audit file destination exists
    if [ ! -d $ORACLE_BASE/admin/$ORACLE_SID/adump ]; then
       mkdir -p $ORACLE_BASE/admin/$ORACLE_SID/adump
    fi;
-   
+
    # Start database
    $ORACLE_BASE/$START_FILE;
-   
+
 else
    # Remove database config files, if they exist
    rm -f $ORACLE_HOME/dbs/spfile$ORACLE_SID.ora
    rm -f $ORACLE_HOME/dbs/orapw$ORACLE_SID
    rm -f $ORACLE_HOME/network/admin/tnsnames.ora
-   
+
    # Create database
    $ORACLE_BASE/$CREATE_DB_FILE $ORACLE_SID $ORACLE_PDB;
-   
+
    # Move database operational files to oradata
    moveFiles;
 fi;
@@ -161,6 +161,10 @@ echo "#########################"
 echo "DATABASE IS READY TO USE!"
 echo "#########################"
 
-tail -f $ORACLE_BASE/diag/rdbms/*/*/trace/alert*.log &
-childPID=$!
-wait $childPID
+if [ "$1" == "no-wait" ]; then
+    exit
+else
+    tail -f $ORACLE_BASE/diag/rdbms/*/*/trace/alert*.log &
+    childPID=$!
+    wait $childPID
+fi;
