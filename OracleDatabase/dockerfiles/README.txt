9809e0d2a71f5761206ed0b918cb7b6d77059a50
Michael Schaufelberger <Michael.Schaufelberger@bsi-software.com>
2017-02-28 07:33:59 +0100

    optimize image size by getting binaries in a RUN block instead of COPY,
    add LABEL oracle.version, adjust image name (no PDB)

---
+++ OracleDatabase/dockerfiles/README.txt
@@ -0,0 +1,1 @@
+For 12.1.0.2, adjust the INSTALL_FILE_BASE_URL in the corresponding Dockerfile first.
--- OracleDatabase/dockerfiles/buildDockerImage.sh
+++ OracleDatabase/dockerfiles/buildDockerImage.sh
@@ -1,20 +1,20 @@
 #!/bin/bash
-# 
+#
 # Since: April, 2016
 # Author: gerald.venzl@oracle.com
 # Description: Build script for building Oracle Database Docker images.
-# 
+#
 # DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS HEADER.
-# 
+#
 # Copyright (c) 2014-2016 Oracle and/or its affiliates. All rights reserved.
-# 
+#
 
 usage() {
   cat << EOF
 
 Usage: buildDockerImage.sh -v [version] [-e | -s | -x] [-i]
 Builds a Docker Image for Oracle Database.
-  
+
 Parameters:
    -v: version to build
        Choose one of: $(for i in $(ls -d */); do echo -n "${i%%/}  "; done)
@@ -61,7 +61,7 @@ ENTERPRISE=0
 STANDARD=0
 EXPRESS=0
 VERSION="12.1.0.2"
-SKIPMD5=0
+SKIPMD5=1
 DOCKEROPS=""
 
 while getopts "hesxiv:" optname; do
@@ -107,7 +107,7 @@ else
 fi
 
 # Oracle Database Image Name
-IMAGE_NAME="oracle/database:$VERSION-$EDITION"
+IMAGE_NAME="oracle-nopdb:$VERSION-$EDITION"
 
 # Go into version folder
 cd $VERSION
@@ -162,12 +162,12 @@ echo ""
 
 if [ $? -eq 0 ]; then
 cat << EOF
-  Oracle Database Docker Image for '$EDITION' version $VERSION is ready to be extended: 
-    
+  Oracle Database Docker Image for '$EDITION' version $VERSION is ready to be extended:
+
     --> $IMAGE_NAME
 
   Build completed in $BUILD_ELAPSED seconds.
-  
+
 EOF
 
 else
